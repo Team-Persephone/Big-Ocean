d0d5885298d19ce291a390eed52a146cd6129f22
Isabelle Stettler <aysa.stettler@gmail.com>
2021-03-17 16:26:12 -0400

    feat: create initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+delete from our package.json at start:
+    "prepare": "if [ -d .git ]; then npm-merge-driver install; fi",
---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+export default {
+    type: Phaser.AUTO,
+
+    width: 800,
+    height: 600,
+    render: {
+        pixelArt: true,
+    },
+    scale: {
+        parent: 'big-ocean',
+        autoCenter: true,
+    },
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        },
+    },
+    dom: {
+        createContainer: true,
+    },
+    scene: [],
+}
