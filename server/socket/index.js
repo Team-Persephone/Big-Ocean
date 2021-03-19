1ca91819b94df18810cda402bd79581700e64624
Olivia Wong <oliviawongnyc@gmail.com>
2021-03-19 13:02:27 -0400

    Feature: set up activeGames object

---
+++ server/socket/index.js
@@ -0,0 +1,22 @@
+const { PearlQuest, ShrimpFact, Level, GameRoom } = require('../db/models');
+
+const activeGames = {
+  // [urlCode]: {
+  //   position: { 'name': [x, y] },
+  //   score: { name: 0 },
+  //   avatars: ['red', 'blue', 'green'],
+  //   level: 1,
+  //   question: {
+  //     q1: {position: [x, y], isResolved: false},
+  //     q2: {position: [x, y], isResolved: false},
+  //     q3: {position: [x, y], isResolved: false},
+  //     q4: {position: [x, y], isResolved: false},
+  //     q5: {position: [x, y], isResolved: false},
+  //     f1: {position: [x, y], isResolved: false},
+  //     f2: {position: [x, y], isResolved: false},
+  //     f3: {position: [x, y], isResolved: false},
+  //     f4: {position: [x, y], isResolved: false},
+  //     f5: {position: [x, y], isResolved: false},
+  //   },
+  // },
+};
