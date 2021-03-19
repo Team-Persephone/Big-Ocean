94bf54e50abea12443f24ce8ef471dc8496e4daa
Isabelle Stettler <aysa.stettler@gmail.com>
2021-03-19 15:39:53 -0400

    feat add tilesheet background

--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -6,15 +6,35 @@ export default class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    console.log('hello')
-    this.load.image('woman', '/img/woman h1.png')
+    this.load.image('woman', '/assets/woman h1.png')
+    this.load.image('tiles', '/assets/ocean-tilesheet.png')
+    this.load.tilemapTiledJSON('tilemap', '/assets/big-ocean-level1.json')
 
 }
 create(){
-    this.add.image(100, 200, 'woman')
+  
+  this.socket = io();
+  this.scene.launch('WaitingRoom', { socket: this.socket})
+
+  const map = this.make.tilemap({ key: 'tilemap' })
+  const tileset = map.addTilesetImage('ocean-scene', 'tiles')
+
+  map.createStaticLayer('water', tileset)
+  map.createStaticLayer('rocklevel1', tileset)
+  map.createStaticLayer('rocklevel2', tileset)
+  map.createStaticLayer('seeweed', tileset)
+
+  this.add.image(100, 200, 'woman')
+
 }
 
   update(){
 
   }
 }
+
+//SET SCREEN SIZE
+// var windowWidth = window.innerWidth;
+// var widnowHeight = window.innerHeight;
+// this.bg = this.add.image(windowWidth / 2, widnowHeight / 2, 'sky');
+// this.bg.setDisplaySize(windowWidth, widnowHeight);
