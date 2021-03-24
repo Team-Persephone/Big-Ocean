c295ab802372078a218cdefef9228a486eafe355
Olivia Wong <oliviawongnyc@gmail.com>
2021-03-24 19:50:43 -0400

    Feature: Four of five clams move

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,5 +1,4 @@
-
-const { PearlQuest, ShrimpFact, Level } = require('../db/models');
+const { PearlQuest, ShrimpFact, Level } = require("../db/models");
 
 const activeGames = {};
 
@@ -14,131 +13,150 @@ const activeGames = {};
     numPlayers: 0
     score: { socket.id: playerScore = 0},
     level: 1,
-    questionsLevel1: [{question: '', options: [], answer: '', positionX: 0, positionY: 0}],
-    questionsLevel2: [{question: '', options: [], answer: '', positionX: 0, positionY: 0}],
-    questionsLevel3: [{question: '', options: [], answer: '', positionX: 0, positionY: 0}],
-    questionsLevel4: [{question: '', options: [], answer: '', positionX: 0, positionY: 0}],
-    questionsLevel5: [{question: '', options: [], answer: '', positionX: 0, positionY: 0}],
+    questionsLevel1: [{question: '', options: [], answer: '', x: 0, y: 0}],
+    questionsLevel2: [{question: '', options: [], answer: '', x: 0, y: 0}],
+    questionsLevel3: [{question: '', options: [], answer: '', x: 0, y: 0}],
+    questionsLevel4: [{question: '', options: [], answer: '', x: 0, y: 0}],
+    questionsLevel5: [{question: '', options: [], answer: '', x: 0, y: 0}],
     facts: []
     //
   }*/
 
-module.exports = (io) => {
-  io.on("connection", async (socket) => {
-    console.log(
-      `A socket connection to the server has been made: ${socket.id}`
-    );
+module.exports = io => {
+	io.on("connection", async socket => {
+		console.log(
+			`A socket connection to the server has been made: ${socket.id}`
+		);
 
-    //socket listen on createGame
-    socket.on('createGame', async function () {
-      let key = codeGenerator();
-      while (Object.keys(activeGames).includes(key)) {
-        key = codeGenerator();
-      }
-      activeGames[key] = {
-        key,
-        players: {},
-        numPlayers: 0,
-        avatars: ['scubaGreen', 'scubaPink', 'scubaPurple'],
-        score: {},
-        level: 1,
-        questions: [],
-        facts: [],
-        // taskPositions: {},
-      };
+		//socket listen on createGame
+		socket.on("createGame", async function () {
+			let key = codeGenerator();
+			while (Object.keys(activeGames).includes(key)) {
+				key = codeGenerator();
+			}
+			activeGames[key] = {
+				key,
+				players: {},
+				numPlayers: 0,
+				avatars: ["scubaGreen", "scubaPink", "scubaPurple"],
+				score: {},
+				level: 1,
+				questionsLevel1: [],
+				questionsLevel2: [],
+				questionsLevel3: [],
+				questionsLevel4: [],
+				questionsLevel5: [],
+				facts: []
+				// taskPositions: {},
+			};
 
-      const questions = await PearlQuest.findAll();
+			const questions = await PearlQuest.findAll();
 
-      // console.log(questions);
+			// console.log(questions);
 
-      let questionsObj = questions.forEach((question) => {
-        let x = Math.ceil(Math.random() * 700);
-        let y = Math.ceil(Math.random() * 500);
-        return {
-          question: question.question,
-          options: question.options,
-          answer: question.answer,
-          postionX: x,
-          positionY: y,
-        };
-      });
+			questions.forEach(question => {
+				let x = Math.ceil(Math.random() * 700);
+				let y = Math.ceil(Math.random() * 500);
+				let questionObj = {
+					question: question.question,
+					options: question.options,
+					answer: question.answer,
+					isResolved: false,
+					x,
+					y
+				};
+				if (question.levelId === 1) {
+					activeGames[key].questionsLevel1.push(questionObj);
+				}
+				if (question.levelId === 2) {
+					activeGames[key].questionsLevel2.push(questionObj);
+				}
+				if (question.levelId === 3) {
+					activeGames[key].questionsLevel3.push(questionObj);
+				}
+				if (question.levelId === 4) {
+					activeGames[key].questionsLevel4.push(questionObj);
+				}
+				if (question.levelId === 5) {
+					activeGames[key].questionsLevel5.push(questionObj);
+				}
+			});
 
-      console.log(questionsObj);
-      socket.emit("gameCreated", key);
-    });
+			console.log(activeGames[key].questionsLevel5);
 
-    //socket listen on plaery joinGame
+			// console.log(questionsObj);
+			socket.emit("gameCreated", key);
+		});
 
-    socket.on('joinWaitingRoom', async function (gameKey) {
-      socket.join(gameKey); //WHAT IS THIS DOING??
-      console.log('this is socket.id', socket.id);
+		//socket listen on plaery joinGame
 
-      const gameInfo = activeGames[gameKey];
-      const newAvatar = activeGames[gameKey].avatars.pop();
-      activeGames[gameKey].players[socket.id] = {
-        position: {
-          x: 100,
-          y: 100,
+		socket.on("joinWaitingRoom", async function (gameKey) {
+			socket.join(gameKey); //WHAT IS THIS DOING??
+			console.log("this is socket.id", socket.id);
 
-          angle: 0,
-          //   faceRight: false,
+			const gameInfo = activeGames[gameKey];
+			const newAvatar = activeGames[gameKey].avatars.pop();
+			activeGames[gameKey].players[socket.id] = {
+				position: {
+					x: 100,
+					y: 100,
 
-        },
-        avatar: newAvatar,
-        playerId: socket.id,
-      };
+					angle: 0
+					//   faceRight: false,
+				},
+				avatar: newAvatar,
+				playerId: socket.id
+			};
 
-      gameInfo.numPlayer = Object.keys(gameInfo.players).length;
-      gameInfo.score[socket.id] = 0;
-      //send state info
+			gameInfo.numPlayer = Object.keys(gameInfo.players).length;
+			gameInfo.score[socket.id] = 0;
+			//send state info
 
-      socket.emit("setState", gameInfo);
+			socket.emit("setState", gameInfo);
 
-      //send current players info
-      socket.emit("currentPlayers", {
-        players: gameInfo.players,
-        numPlayers: gameInfo.numPlayers,
-      });
-      //send new player info
-      socket.to(gameKey).emit("newPlayer", {
-        newPlayer: gameInfo.players[socket.id],
-        numPlayers: gameInfo.numPlayers,
-      });
+			//send current players info
+			socket.emit("currentPlayers", {
+				players: gameInfo.players,
+				numPlayers: gameInfo.numPlayers
+			});
+			//send new player info
+			socket.to(gameKey).emit("newPlayer", {
+				newPlayer: gameInfo.players[socket.id],
+				numPlayers: gameInfo.numPlayers
+			});
 
-    socket.on("submitMemo", async function (key, username, message){
-      console.log("key", key);
-      console.log("userColor", username);
-      console.log("message", message);
-      socket.to(key).emit("broadcastMessage", {
-        username: username,
-        message: message
-      });
-      socket.emit("broadcastMessage", {
-        username: username,
-        message: message
-      })
-    });
+			socket.on("submitMemo", async function (key, username, message) {
+				console.log("key", key);
+				console.log("userColor", username);
+				console.log("message", message);
+				socket.to(key).emit("broadcastMessage", {
+					username: username,
+					message: message
+				});
+				socket.emit("broadcastMessage", {
+					username: username,
+					message: message
+				});
+			});
 
-    //Player Movement
-    socket.on('playerMovement', async function (data) {
-      const { x, y, angle, faceRight, key } = data;
-      activeGames[key].players[socket.id].position.x = x;
-      activeGames[key].players[socket.id].position.y = y;
-      activeGames[key].players[socket.id].position.angle = angle;
-      activeGames[key].players[socket.id].position.faceRight = faceRight;
-      socket.to(key).emit('friendMoved', activeGames[key].players[socket.id]);
+			//Player Movement
+			socket.on("playerMovement", async function (data) {
+				const { x, y, angle, faceRight, key } = data;
+				activeGames[key].players[socket.id].position.x = x;
+				activeGames[key].players[socket.id].position.y = y;
+				activeGames[key].players[socket.id].position.angle = angle;
+				activeGames[key].players[socket.id].position.faceRight = faceRight;
+				socket.to(key).emit("friendMoved", activeGames[key].players[socket.id]);
+			});
+		});
+	});
 
-    });
-  });
-});
-
-
-function codeGenerator() {
-  let code = "";
-  let chars = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
-  for (let i = 0; i < 5; i++) {
-    code += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return code;
-}
-}
+	function codeGenerator() {
+		let code = "";
+		let chars = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
+		for (let i = 0; i < 5; i++) {
+			code += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		return code;
+	}
+};
--- src/scenes/Mainscene.js
+++ src/scenes/Mainscene.js
@@ -1,176 +1,230 @@
-import Phaser from 'phaser';
-import Scuba from '../entities/Scuba';
+import Phaser from "phaser";
+import Scuba from "../entities/Scuba";
+import Clam from "../entities/Clam";
 
 export default class MainScene extends Phaser.Scene {
-  constructor() {
-    super('MainScene');
-    //state will be used to hold socket info
-    this.state = {}
-  }
+	constructor() {
+		super("MainScene");
+		//state will be used to hold socket info
+		this.state = {};
+	}
 
-  // THIS IS PHASER PRELOAD FUCNTION TO LOAD ALL FILES NEEDED TO CREATE SCENE
-  preload() {
+	// THIS IS PHASER PRELOAD FUCNTION TO LOAD ALL FILES NEEDED TO CREATE SCENE
+	preload() {
+		// all avatars are loaded
+		this.load.spritesheet("scubaPink", "/assets/scuba_divers/scubaPink.png", {
+			frameWidth: 820,
+			frameHeight: 420
+		});
+		this.load.spritesheet("scubaGreen", "/assets/scuba_divers/scubaGreen.png", {
+			frameWidth: 820,
+			frameHeight: 420
+		});
+		this.load.spritesheet(
+			"scubaPurple",
+			"/assets/scuba_divers/scubaPurple.png",
+			{
+				frameWidth: 820,
+				frameHeight: 420
+			}
+		);
+		this.load.spritesheet("clam", "/assets/clam.png", {
+			frameWidth: 990,
+			frameHeight: 860
+		});
 
-    // all avatars are loaded
-    this.load.spritesheet('scubaPink', '/assets/scuba_divers/scubaPink.png', {
-      frameWidth: 820,
-      frameHeight: 420,
-    });
-    this.load.spritesheet('scubaGreen', '/assets/scuba_divers/scubaGreen.png', {
-      frameWidth: 820,
-      frameHeight: 420,
-    });
-    this.load.spritesheet('scubaPurple','/assets/scuba_divers/scubaPurple.png', {
-        frameWidth: 820,
-        frameHeight: 420,
-      }
-    );
+		// our background scene is loaded
+		this.load.image("tiles", "/assets/ocean-tilesheet.png");
+		this.load.tilemapTiledJSON("tilemap", "/assets/big-ocean-level1.json");
 
-    // our background scene is loaded
-    this.load.image('tiles', '/assets/ocean-tilesheet.png');
-    this.load.tilemapTiledJSON('tilemap', '/assets/big-ocean-level1.json');
+		//Audio
+		this.load.audio("music", ["/audio/Waiting_Room.mp3"]);
+	}
 
-    //Audio
-    this.load.audio('music', ['/audio/Waiting_Room.mp3']);
-  }
+	//helper function to create avatar for player
+	createPlayer(scene, player) {
+		scene.scubaDiver = new Scuba(this, 100, 200, `${player.avatar}`).setScale(
+			0.2
+		);
+		scene.scubaDiver.setAngle(-45);
+		scene.scubaDiver.faceRight = true;
+		//scuba can't leave the screne
+		scene.scubaDiver.body.collideWorldBounds = true;
+		this.createAnimations(player.avatar);
+	}
 
-  //helper function to create avatar for player
-  createPlayer (scene, player) {
-    scene.scubaDiver = new Scuba(this, 100, 200, `${player.avatar}`).setScale(0.2);
-    scene.scubaDiver.setAngle(-45);
-    scene.scubaDiver.faceRight = true;
-    //scuba can't leave the screne
-    scene.scubaDiver.body.collideWorldBounds = true;
+	createClam(scene, x, y, file) {
+		scene.clam = new Clam(this, x, y, file).setScale(0.07);
+		console.log(scene.clam);
+		this.createAnimationsClam("clam");
+	}
 
-    this.createAnimations(player.avatar)
+	// helper function to add animation to avatars
+	createAnimations(avatar) {
+		this.anims.create({
+			key: "swim",
+			frames: this.anims.generateFrameNumbers(avatar, {
+				start: 5,
+				end: 9
+			}),
+			frameRate: 5,
+			repeat: -1
+		});
+	}
 
-  }
+	createAnimationsClam(object) {
+		this.anims.create({
+			key: "openclose",
+			frames: this.anims.generateFrameNumbers(object, {
+				start: 0,
+				end: 2
+			}),
+			frameRate: 1,
+			repeat: -1
+		});
+	}
 
-  // helper function to add animation to avatars
-  createAnimations(avatar) {
-    this.anims.create({
-      key: 'swim',
-      frames: this.anims.generateFrameNumbers(avatar, {
-        start: 5,
-        end: 9,
-      }),
-      frameRate: 5,
-      repeat: -1,
-    });
-  }
+	//helper function to add other players to scene
+	addFriends(scene, player) {
+		const playerFriend = scene.add
+			.sprite(
+				player.position.x + 40,
+				player.position.y + 40,
+				`${player.avatar}`
+			)
+			.setScale(0.2);
+		playerFriend.faceRight = true;
+		playerFriend.playerId = player.playerId;
+		scene.playerFriends.add(playerFriend);
+	}
 
-  //helper function to add other players to scene
-  addFriends(scene, player) {
-    const playerFriend = scene.add.sprite(
-      player.position.x + 40,
-      player.position.y + 40,
-      `${player.avatar}`
-    ).setScale(0.2);
-    playerFriend.faceRight = true;
-    playerFriend.playerId = player.playerId;
-    scene.playerFriends.add(playerFriend);
-  }
+	// THIS IS PHASER CREATE FUNCTION TO CREATE SCENE
+	create() {
+		const scene = this;
 
- // THIS IS PHASER CREATE FUNCTION TO CREATE SCENE
-  create() {
-    const scene = this;
+		this.music = this.sound.add("music", {
+			volume: 0.5,
+			loop: true
+		});
+		this.music.play();
 
-    this.music = this.sound.add('music', {
-      volume: 0.5,
-      loop: true,
-    });
-    this.music.play();
+		//launch the socket connection
+		this.socket = io();
+		//connect the socket connection to the WaitingRoom
 
-    //launch the socket connection
-    this.socket = io();
-    //connect the socket connection to the WaitingRoom
+		// const clam = this.scene.add.sprite(0, 0, "clam");
 
-    this.scene.launch('WaitingRoom', { socket: this.socket });
-    this.scene.launch('ChatScene', { socket: this.socket });
-    this.scene.launch('IntroScene', { socket: this.socket });
+		// this.scene.clams.add(clam);
 
-    scene.playerFriends = this.physics.add.group(); //---> WHAT DOES THIS AND IS THIS CORRECTLY IMPLIED FOR OUR PROJECT?!
-    // create scene from tilemap
-    const map = this.make.tilemap({ key: 'tilemap' });
-    const tileset = map.addTilesetImage('ocean-scene', 'tiles');
+		this.scene.launch("WaitingRoom", { socket: this.socket });
+		this.scene.launch("ChatScene", { socket: this.socket });
+		this.scene.launch("IntroScene", { socket: this.socket });
 
-    map.createStaticLayer('water', tileset);
-    map.createStaticLayer('rocklevel1', tileset);
-    map.createStaticLayer('rocklevel2', tileset);
-    map.createStaticLayer('seeweed', tileset);
+		scene.playerFriends = this.physics.add.group(); //---> WHAT DOES THIS AND IS THIS CORRECTLY IMPLIED FOR OUR PROJECT?!
+		// scene.clams = this.physics.add.group();
+		// create scene from tilemap
+		const map = this.make.tilemap({ key: "tilemap" });
+		const tileset = map.addTilesetImage("ocean-scene", "tiles");
 
+		map.createStaticLayer("water", tileset);
+		map.createStaticLayer("rocklevel1", tileset);
+		map.createStaticLayer("rocklevel2", tileset);
+		map.createStaticLayer("seeweed", tileset);
 
-    //create navigation and animation for scuba divers
-    this.cursors = this.input.keyboard.createCursorKeys();
+		const playButton = this.add.text(400, 500, "Play", { fontFamily: "menlo" });
+		playButton.setInteractive();
+		playButton.on("pointerdown", () => {
+			playButton.setVisible(false);
+			scene.state.questionsLevel1.forEach(question => {
+				scene.createClam(scene, question.x, question.y, "clam");
+			});
+		});
 
-    //Volume - add volume sound bar for display here
+		//create navigation and animation for scuba divers
+		this.cursors = this.input.keyboard.createCursorKeys();
 
-    // this.createPlayer(gameInfo.players[socketId])
-    this.socket.on('setState', function(gameInfo){
-      const { key, players, avatars, score, level, questions, facts } = gameInfo;
-      //this.physics.resume() ----> WHAT DOES THIS??
+		//Volume - add volume sound bar for display here
 
-      //set state to gameInfo
-      scene.state.key = key;
-      scene.state.players = players;
-      scene.state.avatars = avatars;
-      scene.state.score = score;
-      scene.state.level = level;
-      scene.state.questions = questions;
-      scene.state.facts = facts;
-    })
+		// this.createPlayer(gameInfo.players[socketId])
+		this.socket.on("setState", function (gameInfo) {
+			const {
+				key,
+				players,
+				avatars,
+				score,
+				level,
+				questionsLevel1,
+				questionsLevel2,
+				questionsLevel3,
+				questionsLevel4,
+				questionsLevel5,
+				facts
+			} = gameInfo;
+			//this.physics.resume() ----> WHAT DOES THIS??
 
-    this.socket.on('currentPlayers', function({ players, numPlayers }) {
-      scene.state.numPlayers = numPlayers;
-      Object.keys(players).forEach( function(id){
-        if(players[id].playerId === scene.socket.id){
-          scene.createPlayer(scene, players[id])
-        } else {
-          scene.addFriends(scene, players[id]);
-        }
-      });
-    });
-    //listen to add new player to scene
-    this.socket.on('newPlayer', function({ newPlayer, numPlayers }) {
-      scene.addFriends(scene, newPlayer);
-      scene.state.numPlayers = numPlayers;
+			//set state to gameInfo
+			scene.state.key = key;
+			scene.state.players = players;
+			scene.state.avatars = avatars;
+			scene.state.score = score;
+			scene.state.level = level;
+			scene.state.questionsLevel1 = questionsLevel1;
+			scene.state.questionsLevel2 = questionsLevel2;
+			scene.state.questionsLevel3 = questionsLevel3;
+			scene.state.questionsLevel4 = questionsLevel4;
+			scene.state.questionsLevel5 = questionsLevel5;
+			scene.state.facts = facts;
 
+			// console.log("state -->", scene.state);
+		});
 
-    })
-
-
-    this.socket.on("friendMoved", function (friend) {
-			scene.playerFriends.getChildren().forEach(function (playerFriend) {
-				if (friend.playerId === playerFriend.playerId) {
-          const previousX = playerFriend.x;
-					const previousY = playerFriend.y;
-          const previousAngle = playerFriend.angle;
-          const previousFaceRight = playerFriend.faceRight;
-
-          if(previousX !== friend.position.x ) {
-            playerFriend.x = friend.position.x;
-          }
-          if(previousY !== friend.position.y){
-            playerFriend.y = friend.position.y;
-          }
-          if(previousAngle !== friend.position.angle){
-            playerFriend.angle = friend.position.angle;
-          }
-          if(previousFaceRight !== friend.position.faceRight){
-            playerFriend.flipX = !playerFriend.flipX;
-            playerFriend.faceRight = friend.position.faceRight;
-           }
+		this.socket.on("currentPlayers", function ({ players, numPlayers }) {
+			scene.state.numPlayers = numPlayers;
+			Object.keys(players).forEach(function (id) {
+				if (players[id].playerId === scene.socket.id) {
+					scene.createPlayer(scene, players[id]);
+				} else {
+					scene.addFriends(scene, players[id]);
 				}
 			});
 		});
-  }
+		//listen to add new player to scene
+		this.socket.on("newPlayer", function ({ newPlayer, numPlayers }) {
+			scene.addFriends(scene, newPlayer);
+			scene.state.numPlayers = numPlayers;
+		});
 
-  update() {
-    const scene = this;
-    //update the movement
-    if(this.scubaDiver) {
-      this.scubaDiver.update(this.cursors);
-    }
+		this.socket.on("friendMoved", function (friend) {
+			scene.playerFriends.getChildren().forEach(function (playerFriend) {
+				if (friend.playerId === playerFriend.playerId) {
+					const previousX = playerFriend.x;
+					const previousY = playerFriend.y;
+					const previousAngle = playerFriend.angle;
+					const previousFaceRight = playerFriend.faceRight;
 
-  }
+					if (previousX !== friend.position.x) {
+						playerFriend.x = friend.position.x;
+					}
+					if (previousY !== friend.position.y) {
+						playerFriend.y = friend.position.y;
+					}
+					if (previousAngle !== friend.position.angle) {
+						playerFriend.angle = friend.position.angle;
+					}
+					if (previousFaceRight !== friend.position.faceRight) {
+						playerFriend.flipX = !playerFriend.flipX;
+						playerFriend.faceRight = friend.position.faceRight;
+					}
+				}
+			});
+		});
+	}
+
+	update() {
+		const scene = this;
+		//update the movement
+		if (this.scubaDiver) {
+			this.scubaDiver.update(this.cursors);
+		}
+	}
 }
