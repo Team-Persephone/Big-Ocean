15010aedd38bf12b148b35936046b65b720b1b8d
SofijaSutton <srlsutton@gmail.com>
2021-03-26 15:09:25 -0400

    Feat: add bubble behind instructions

--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -10,21 +10,23 @@ export default class Instructions extends Phaser.Scene {
 	}
 
   preload() {
-
+    this.load.image("bubble-large", "/assets/bubble-large.png");
   }
 
   async create() {
     const scene = this;
 
-    scene.add.text(275, 10, "instructiOns", {
+    scene.graphics = scene.add.image(400, 165, "bubble-large").setScale(.525);
+
+    scene.add.text(275, 40, "instructiOns", {
       fill: "#00ff00",
-      backgroundColor: "#1abeff",
+     // backgroundColor: "#1abeff",
       fontSize: "34px",
       fontStyle: "bold",
     });
 
-    scene.returnText = scene.add.text(650, 270, "gO back", {
-      fill: "#ffffff",
+    scene.returnText = scene.add.text(350, 280, "gO back", {
+      fill: "#00ff00",
       fontSize: "17px",
       fontStyle: "bold",
     });
@@ -35,10 +37,10 @@ export default class Instructions extends Phaser.Scene {
     scene.returnText.setInteractive();
 
     scene.returnText.on("pointerover", () => {
-      scene.returnText.setStyle({ fill: '#0f0'});
+      scene.returnText.setStyle({ fill: '#4080ff'});
     });
     scene.returnText.on("pointerout", () => {
-      scene.returnText.setStyle({ fill: '#fff'});
+      scene.returnText.setStyle({ fill: '#0f0'});
     });
 
     scene.returnText.on("pointerdown", () => {
@@ -48,11 +50,11 @@ export default class Instructions extends Phaser.Scene {
 
     scene.add.text(
       50,
-      50,
-      "\n\n1. navigate yOur surrOundings + lOcate preciOus clams, whOse pearls cOntain ancient queries On big-O nOtatiOn. \n\n \n 2. dOn’t fOrget tO keep an eye On the clOck. \n \n 3. need help? apprOach a shrimp fOr a hint. \n \n 4. finish with the mOst pOints, and win the title of “mighty O.” \n \n 5. ready tO play? click the buttOn belOw tO cOntinue.\n \n ",
+      80,
+      "\n1. navigate yOur surrOundings + lOcate preciOus clams, whOse pearls cOntain ancient queries On big-O nOtatiOn. \n\n \n 2. dOn’t fOrget tO keep an eye On the clOck. \n \n 3. need help? apprOach a shrimp fOr a hint. \n \n 4. finish with the mOst pOints, and win the title of “mighty O.” \n \n 5. ready tO play? click the buttOn belOw tO cOntinue.\n \n ",
       {
         fill: "#00ff00",
-        backgroundColor: "#1abeff",
+     //   backgroundColor: "#1abeff",
         fontSize: "17px",
         fontStyle: "bold",
         align: "left",
