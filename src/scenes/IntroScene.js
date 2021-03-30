f6515f8d2dcde35e6a93c8045867e27636baded5
Olivia Wong <oliviawongnyc@gmail.com>
2021-03-30 13:03:02 -0400

    Player cannot be added after 3 already in game

--- src/scenes/IntroScene.js
+++ src/scenes/IntroScene.js
@@ -3,6 +3,7 @@ import Phaser from "phaser";
 export default class IntroScene extends Phaser.Scene {
 	constructor() {
 		super("IntroScene");
+		this.gameFull = {};
 	}
 	//get scoket data as props from MainRoom
 	init(data) {
@@ -24,12 +25,22 @@ export default class IntroScene extends Phaser.Scene {
 
 		//if there is a gameKey in the url, stop the waiting room because the code was already generated
 		if (window.location.pathname.length > 1) {
-			//gets gameKey from url
 			key = window.location.pathname.slice(1);
-			this.socket.emit("joinWaitingRoom", key);
-			this.scene.launch("Instructions");
-			this.scene.stop("IntroScene");
-			// return;
+			console.log("key up top -->", key);
+			if (!this.gameFull[key]) {
+				console.log("hit1");
+				this.add.text(600, 450, "game already full", {
+					fontFamily: "menlo"
+				});
+			} else {
+				console.log("hitting");
+				this.gameFull[key] = false;
+				//gets gameKey from url
+				this.socket.emit("joinWaitingRoom", key);
+				this.scene.launch("Instructions");
+				this.scene.stop("IntroScene");
+				// return;
+			}
 		}
 		//OTHERWISE THIS:
 		// add button to create game to scene
@@ -49,21 +60,12 @@ export default class IntroScene extends Phaser.Scene {
 			this.scene.stop("Instructions");
 		});
 
-		// 	const joinGameButton = this.add.text(600, 500, "jOin game!", {
-		// 		fontFamily: "menlo"
-		// 	});
-		// 	joinGameButton.setVisible(false);
-		// 	joinGameButton.setInteractive();
-		// 	joinGameButton.on("pointerdown", () => {
-		// 		joinGameButton.setVisible(false);
-
-		// 	});
+		this.socket.on("gameFull", key => {
+			console.log("this.gameFull -->", this.gameFull);
+			this.gameFull[key] = true;
+			console.log("this.gameFull after -->", this.gameFull);
+		});
 	}
 
 	update() {}
 }
-
-// server: when socket connection is made, (aka, when someone lands on the intro) write a function to create a unique code and emit that code back to the frontend
-// client: when the frontend receives the unique code, the unique code is appended to the a href on the "play" button, aka: one.big.ocean.herokuapp.com/8D4F3
-// one.big.ocean.herokuapp.com/8D4F3 brings user to the waitingroom
-// user can send the unique URL to friends to join the same waiting room.
