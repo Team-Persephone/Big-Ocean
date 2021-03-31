c69b2d90354e688e0eb29761d8168cba58d56e5a
Olivia Wong <oliviawongnyc@gmail.com>
2021-03-31 19:28:12 -0400

    Fixes game full scene

--- src/scenes/GameFull.js
+++ src/scenes/GameFull.js
@@ -16,6 +16,8 @@ export default class GameFull extends Phaser.Scene {
 	async create() {
 		const scene = this;
 
+		this.add.image(0, 0, "blue").setScale(2);
+
 		scene.graphics = scene.add.image(400, 260, "bubble").setScale(0.9);
 
 		scene.add.text(220, 250, "sOrry, this game is full", {
@@ -24,22 +26,18 @@ export default class GameFull extends Phaser.Scene {
 			fontStyle: "bold"
 		});
 
-		const createGameButton = this.add.text(330, 320, "gO back hOme", {
+		const createGameButton = this.add.text(320, 320, "create new game", {
 			fill: "#02075D",
 			fontSize: "20px",
 			fontStyle: "bold"
 		});
+
 		createGameButton.setInteractive();
 		createGameButton.on("pointerdown", () => {
 			window.open(
 				`${window.location.href.slice(0, window.location.href.length - 6)}`,
 				"_blank"
 			);
-		});
-
-		this.socket.on("gameCreated", key => {
-			this.socket.emit("joinWaitingRoom", key);
-			this.scene.stop("GameFull");
 		});
 	}
 }
